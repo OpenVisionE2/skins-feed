d765397af98a5fd2be066a31d8661ff661cc4126
persianpros <persianpros@gmail.com>
2020-11-08 20:16:12 +0330

    Add missing files

--- ts.sh
+++ ts.sh
@@ -2,10 +2,9 @@
 
 mkdir -p ts
 cd ts
-wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins/
-wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_IPTV/
-wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_Backup/
-wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Plugins_Multiboot/
+wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Skins-HD/
+wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Skins-FHD/
+wget -c -nH -r -q -l0 -A ipk -np -nd --wait=3 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/TS-Skins/
 rename 'y/A-Z/a-z/' *.ipk
 mv -f *.ipk ../
 cd ..
