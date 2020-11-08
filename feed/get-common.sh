0b60bec76e4156faefbbbd13c46a4599dd3867c2
Persian Prince <persianpros@yahoo.com>
2020-11-09 02:36:09 +0330

    Update get-common.sh

--- feed/get-common.sh
+++ feed/get-common.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 rename 'y/A-Z/a-z/' *.ipk
-#ls -1 | grep .ipk | grep -v '_all' | xargs rm -f
+#mkdir -p files
+#mv -f *.ipk files/
+#cd files
+#ls -1  | grep -v '_all' | xargs rm -f
+#mv -f *.ipk ../
+#cd ..
+#rm -rf files
 ls -vr *.ipk | awk -F- '$1 == name{system ("rm \""$0"\"")}{name=$1}'
 find . -name '*picons*' -type f | xargs rm -f
