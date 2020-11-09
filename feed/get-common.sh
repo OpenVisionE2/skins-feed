e1e80ef33771c4dbde6deeb9c2fc8804dfac93f6
Persian Prince <persianpros@yahoo.com>
2020-11-09 11:06:57 +0330

    We can't upload more than 100MB to github

--- feed/get-common.sh
+++ feed/get-common.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
 rename 'y/A-Z/a-z/' *.ipk
-#ls -1 | grep -v '_all' | xargs rm -f
-ls -vr *.ipk | awk -F- '$1 == name{system ("rm \""$0"\"")}{name=$1}'
-find . -name '*plugin-picons-*' -type f | xargs rm -f
+find . -name '*picons*' -type f | xargs rm -f
+#mkdir -p files
+#mv -f *.ipk files/
+#cd files
+#ls -1  | grep -v '_all' | xargs rm -f
+#mv -f *.ipk ../
+#cd ..
+#rm -rf files
+for file in $(ls *.ipk | awk -F'_' '{print $1}' | sort -u)
+do
+    count=$(ls ${file}* | wc -l)
+    if [ ${count} -gt 1 ]; then
+        ls -rv ${file}* | tail -$(($count-1)) | xargs rm
+    fi
+done
--- feed/get-ts.sh
+++ feed/get-ts.sh
@@ -2,9 +2,21 @@
 
 mkdir -p ts
 cd ts
+mkdir -p skinshd
+cd skinshd
 wget -c -nH -r -q -l0 -A ipk -np -nd --wait=1 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Skins-HD/
+mv -f *.ipk ../
+cd ..
+mkdir -p skinsfhd
+cd skinsfhd
 wget -c -nH -r -q -l0 -A ipk -np -nd --wait=1 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/Skins-FHD/
+mv -f *.ipk ../
+cd ..
+mkdir -p skins
+cd skins
 wget -c -nH -r -q -l0 -A ipk -np -nd --wait=1 --execute="robots = off" http://tunisia-dreambox.info/TSipanel/TS-Skins/
+mv -f *.ipk ../
+cd ..
 mv -f *.ipk ../
 cd ..
 rm -rf ts
