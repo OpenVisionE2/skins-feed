d437240adee1cd0cfe8cf79a2ce6e847d4f0ca4d
Persian Prince <persianpros@yahoo.com>
2020-11-09 11:03:21 +0330

    Update get-common.sh

--- feed/get-common.sh
+++ feed/get-common.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rename 'y/A-Z/a-z/' *.ipk
+find . -name '*picons*' -type f | xargs rm -f
 #mkdir -p files
 #mv -f *.ipk files/
 #cd files
@@ -8,5 +9,10 @@ rename 'y/A-Z/a-z/' *.ipk
 #mv -f *.ipk ../
 #cd ..
 #rm -rf files
-ls -vr *.ipk | awk -F- '$1 == name{system ("rm \""$0"\"")}{name=$1}'
-find . -name '*picons*' -type f | xargs rm -f
+for file in $(ls *.ipk | awk -F'_' '{print $1}' | sort -u)
+do
+    count=$(ls ${file}* | wc -l)
+    if [ ${count} -gt 1 ]; then
+        ls -rv ${file}* | tail -$(($count-1)) | xargs rm
+    fi
+done
